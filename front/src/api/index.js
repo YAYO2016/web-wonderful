65e7d21594f5eef904d480c1145d0a46a9cc6f76
YAYO严 <15720826383@163.com>
2020-03-27 22:09:27 +0800

    千年眼  前端  批量导入api文件

--- front/src/api/index.js
+++ front/src/api/index.js
@@ -5,9 +5,16 @@
 import homeApi from './interfaces/homeApi';
 import userApi from './interfaces/userApi';
 
-const api = {
-    ...userApi,
-    ...homeApi
-};
+//批量引入interfaces下的接口文件导入模块（读取文件的路径，是否遍历文件的子目录，匹配文件的正则）
+let requireAll = require.context('./interfaces', false, /\.js$/);
+//requireAll.keys()为文件名数组; requireAll(apiName)获取文件暴露的内容
+const apiArr = requireAll.keys().map(apiName=> requireAll(apiName).default || requireAll(apiName));
+//组合接口
+const api = apiArr.reduce((prev,curr)=> Object.assign(prev,curr), {});
+
+//const api = {
+//    ...userApi,
+//    ...homeApi
+//};
 
 export default api;
--- front/src/store/index.js
+++ front/src/store/index.js
@@ -4,12 +4,12 @@ import getters from './getters'
 import createLogger from 'vuex/dist/logger';
 
 //导入模块
-const modulesFiles = require.context('./modules', true, /\.js$/)
+const modulesFiles = require.context('./modules', true, /\.js$/);
 const modules = modulesFiles.keys().reduce((modules, modulePath) => {
   // set './app.js' => 'app'
-  const moduleName = modulePath.replace(/^\.\/(.*)\.\w+$/, '$1')
-  const value = modulesFiles(modulePath)
-  modules[moduleName] = value.default
+  const moduleName = modulePath.replace(/^\.\/(.*)\.\w+$/, '$1');
+  const value = modulesFiles(modulePath);
+  modules[moduleName] = value.default;
   return modules
 }, {});
 
